ae49d947542a93784a6d62f3f36e0ae2b62f4188
Anna Zadiraka <annazadiraka@gmail.com>
2023-07-24 08:27:09 +0300

    delite btn language

--- js/main.js
+++ js/main.js
@@ -13,33 +13,3 @@ $menuLink.forEach((el) => {
         $menu.classList.toggle('menu__list--active');
     });
 });
-
-// перевод на русский/английский
-const $en = document.querySelectorAll('.en'),
-    $ru = document.querySelectorAll('.ru'),
-    $btnEn = document.querySelectorAll('.btn-en'),
-    $btnRu = document.querySelectorAll('.btn-ru');
-
-    $btnEn.forEach((el) => {
-        el.addEventListener('click', () => {
-            console.log('en');
-            $ru.forEach((el) => {            
-                el.style.display = 'none';
-            });
-            $en.forEach((el) => {
-                el.style.display = 'block';
-            });
-        });
-    });
-
-    $btnRu.forEach((el) => {
-        el.addEventListener('click', () => {
-            console.log('ru');
-            $en.forEach((el) => {
-                el.style.display = 'none';
-            });
-            $ru.forEach((el) => {
-                el.style.display = 'block';
-            });
-        });
-    });
